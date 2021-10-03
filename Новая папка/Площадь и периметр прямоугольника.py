56ba2a728014ac665572ef66b544bdac6ba200a9
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-10-03 15:42:52 +0300

    Площадь и периметр прямоугольника

---
+++ Новая папка/Площадь и периметр прямоугольника.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().split())
+c =(b+a)*2
+d =a*b
+print("The perimeter of the rectangle with sides ",a," and ",b," is ",c,".",sep='')
+print("The area of the rectangle with sides ",a," and ",b," is ",d,".",sep='')
